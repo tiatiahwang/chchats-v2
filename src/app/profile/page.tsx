db0ee2cc362661e0b4a53e3597f7a001ad848942
tiatiahwang <tiahwang44@gmail.com>
2023-07-15 15:08:12 -0700

    fix: error

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,14 @@
 import ProfileNav from '@/components/profile/ProfileNav';
+import { getAuthSession } from '@/lib/auth';
 
-const Page = () => {
-  return <ProfileNav />;
+const Page = async () => {
+  const session = await getAuthSession();
+  return (
+    <>
+      {session?.user ? (
+        <ProfileNav user={session.user} />
+      ) : null}
+    </>
+  );
 };
 export default Page;
--- src/components/profile/ProfileNav.tsx
+++ src/components/profile/ProfileNav.tsx
@@ -1,104 +1,96 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
 import { Icons } from '../Icons';
 import Link from 'next/link';
-import { cls } from '@/lib/utils';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+import { User } from 'next-auth';
+import { FC } from 'react';
 
-const ProfileNav = () => {
-  const { data: session } = useSession();
-  const router = useRouter();
+interface ProfileNavProps {
+  user: User & {
+    username: string;
+  };
+}
+
+const ProfileNav: FC<ProfileNavProps> = ({ user }) => {
   const pathname = usePathname();
 
   return (
     <>
-      {session?.user ? (
-        <>
-          {session?.user ? (
-            <div className='flex items-center justify-between'>
-              <div className='flex space-x-2 items-center'>
-                <div className='cursor-pointer'>
-                  {session.user.image ? (
-                    <div className='relative aspect-square h-8 w-8 rounded-full md:w-10 md:h-10'>
-                      <Image
-                        fill
-                        src={session?.user.image!}
-                        alt='user avatar'
-                        referrerPolicy='no-referrer'
-                        className='rounded-full'
-                      />
-                    </div>
-                  ) : (
-                    <Icons.user className='w-8 h-8 border-[1.5px] p-1 rounded-full border-slate-900 md:w-10 md:h-10' />
-                  )}
+      {user ? (
+        <div className='flex items-center justify-between'>
+          <div className='flex space-x-2 items-center'>
+            <div className='cursor-pointer'>
+              {user.image ? (
+                <div className='relative aspect-square h-8 w-8 rounded-full md:w-10 md:h-10'>
+                  <Image
+                    fill
+                    src={user.image!}
+                    alt='user avatar'
+                    referrerPolicy='no-referrer'
+                    className='rounded-full'
+                  />
                 </div>
-                <div>
-                  <div className='font-semibold'>
-                    {session.user.username}
-                  </div>
-                  <div className='text-gray-500 text-xs'>
-                    {session.user.email}
-                  </div>
-                </div>
+              ) : (
+                <Icons.user className='w-8 h-8 border-[1.5px] p-1 rounded-full border-slate-900 md:w-10 md:h-10' />
+              )}
+            </div>
+            <div>
+              <div className='font-semibold'>
+                {user?.username}
               </div>
-              <div className='space-x-2'>
-                <button
-                  className='p-2 rounded-md bg-main text-white hover:bg-mainDark text-sm'
-                  onClick={() => {
-                    router.push('/profile/edit');
-                  }}
-                >
-                  프로필 수정
-                </button>
+              <div className='text-gray-500 text-xs'>
+                {user?.email}
               </div>
             </div>
-          ) : null}
-          <div className='flex items-center justify-start gap-x-6 border-b-[1px] border-gray-100 text-sm mt-6 font-medium'>
-            <Link href='/profile/mypost' className='pb-4'>
-              <span
-                className={cls(
-                  'pb-4 px-2',
-                  pathname.includes('mypost') ||
-                    pathname.split('/').length === 2
-                    ? 'border-b-[3px] border-main text-main'
-                    : 'hover:border-b-[3px] hover:border-mainDark hover:text-mainDark',
-                )}
-              >
-                내가 쓴 글
-              </span>
-            </Link>
+          </div>
+          <div className='space-x-2'>
             <Link
-              href='/profile/mycomment'
-              className='pb-4'
+              className='p-2 rounded-md bg-main text-white hover:bg-mainDark text-sm'
+              href='/profile/edit'
             >
-              <span
-                className={cls(
-                  'pb-4 px-2',
-                  pathname.includes('mycomment')
-                    ? 'border-b-[3px] border-main text-main'
-                    : 'hover:border-b-[3px] hover:border-mainDark hover:text-mainDark',
-                )}
-              >
-                나의 댓글
-              </span>
-            </Link>
-            <Link href='/profile/myscrap' className='pb-4'>
-              <span
-                className={cls(
-                  'pb-4 px-2',
-                  pathname.includes('myscrap')
-                    ? 'border-b-[3px] border-main text-main'
-                    : 'hover:border-b-[3px] hover:border-mainDark hover:text-mainDark',
-                )}
-              >
-                스크랩
-              </span>
+              프로필 수정
             </Link>
           </div>
-        </>
+        </div>
       ) : null}
+      <div className='flex items-center justify-start gap-x-6 border-b-[1px] border-gray-100 text-sm mt-6 font-medium'>
+        <Link href='/profile/mypost' className='pb-4'>
+          <span
+            className={`${
+              pathname.includes('mypost') ||
+              pathname.split('/').length === 2
+                ? 'border-b-[3px] border-main text-main'
+                : 'hover:border-b-[3px] hover:border-mainDark hover:text-mainDark'
+            } pb-4 px-2`}
+          >
+            내가 쓴 글
+          </span>
+        </Link>
+        <Link href='/profile/mycomment' className='pb-4'>
+          <span
+            className={`${
+              pathname.includes('mycomment')
+                ? 'border-b-[3px] border-main text-main'
+                : 'hover:border-b-[3px] hover:border-mainDark hover:text-mainDark'
+            } pb-4 px-2`}
+          >
+            나의 댓글
+          </span>
+        </Link>
+        <Link href='/profile/myscrap' className='pb-4'>
+          <span
+            className={`${
+              pathname.includes('myscrap')
+                ? 'border-b-[3px] border-main text-main'
+                : 'hover:border-b-[3px] hover:border-mainDark hover:text-mainDark'
+            } pb-4 px-2`}
+          >
+            스크랩
+          </span>
+        </Link>
+      </div>
     </>
   );
 };
