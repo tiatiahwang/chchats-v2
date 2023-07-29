799ee9f51cf1e6b624a315d8d34fea611ec5fb58
tiatiahwang <tiahwang44@gmail.com>
2023-07-29 13:05:09 -0700

    edit: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,9 +15,12 @@ export async function middleware(req: NextRequest) {
   });
   const { pathname } = req.nextUrl;
 
-  // TODO: 로그인 안한 상태에서, 로그인이 필요한 페이지들 이동시 강제 이동 필요함
-  if (pathname.includes('/profile')) {
-    if (!session) {
+  // 로그인 안한 상태에서, 로그인이 필요한 페이지들 이동시 강제 이동
+  if (!session) {
+    if (
+      pathname.includes('/profile') ||
+      pathname.includes('/create')
+    ) {
       return NextResponse.redirect(
         new URL('/login', req.url),
       );
@@ -25,16 +28,23 @@ export async function middleware(req: NextRequest) {
   }
 
   // 로그인한 경우, 로그인 화면과 회원 가입 화면 접근 제한
-  if (
-    pathname.includes('/login') ||
-    pathname.includes('/join')
-  ) {
-    if (session) {
+  if (session) {
+    if (
+      pathname.includes('/login') ||
+      pathname.includes('/join')
+    ) {
       return NextResponse.redirect(new URL('/', req.url));
     }
   }
 }
 
 export const config = {
-  matcher: ['/login', '/join', '/profile', '/profile/edit'],
+  matcher: [
+    '/((?!api|_next|fonts|examples|[\\w-]+\\.\\w+).*)',
+    // '/login',
+    // '/join',
+    // '/profile',
+    // '/profile/edit',
+    // '/:path*/create',
+  ],
 };
