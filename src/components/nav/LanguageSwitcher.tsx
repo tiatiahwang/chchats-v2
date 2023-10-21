089f487e3f4b09195ff98676ac73e22135396ba5
tiatiahwang <tiahwang44@gmail.com>
2023-10-20 20:40:22 -0700

    edit: language select css

--- src/components/nav/LanguageSwitcher.tsx
+++ src/components/nav/LanguageSwitcher.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const LanguageSwitcher = () => {
-  const router = useRouter();
   const pathname = usePathname();
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const redirectedPathname = (locale: string) => {
     if (!pathname) return '/';
@@ -15,16 +16,50 @@ const LanguageSwitcher = () => {
   };
 
   return (
-    <select
-      className='text-sm py-2 pl-2 pr-4 border focus:ring-2 focus:outline-none foucs:ring-main rounded-md cursor-pointer'
-      onChange={(e: any) => {
-        const url = redirectedPathname(e.target.value);
-        router.push(url);
-      }}
-    >
-      <option value='en'>en</option>
-      <option value='ko'>ko</option>
-    </select>
+    <div className='relative text-xs'>
+      <button
+        type='button'
+        className='border focus:ring-1 focus:outline-none hover:border-main focus:ring-main rounded-md px-3 py-2 text-center inline-flex items-center'
+        onClick={() => setShowDropdown((prev) => !prev)}
+      >
+        {pathname.includes('ko') ? '언어선택' : 'language'}
+        <svg
+          className='w-2.5 h-2.5 ml-2.5'
+          aria-hidden='true'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 10 6'
+        >
+          <path
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='1'
+            d='m1 1 4 4 4-4'
+          />
+        </svg>
+      </button>
+      {showDropdown && (
+        <div className='z-50 absolute top-10 bg-white rounded-lg w-full'>
+          <ul className='py-2'>
+            <li className='block px-4 py-2 hover:bg-gray-100 cursor-pointer'>
+              <Link href={redirectedPathname('en')}>
+                {pathname.includes('ko')
+                  ? '영어'
+                  : 'English'}
+              </Link>
+            </li>
+            <li className='block px-4 py-2 hover:bg-gray-100 cursor-pointer'>
+              <Link href={redirectedPathname('ko')}>
+                {pathname.includes('ko')
+                  ? '한국어'
+                  : 'Korean'}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
